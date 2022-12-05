1de17d6d3e8bcd228ae8209eab421eea5a4e7413
Yusuf Babatunde <yusuf06064604@gmail.com>
2022-12-05 00:54:32 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+def new_in_list(my_list, idx, element):
+    if(my_list is not None):
+        m_list = my_list.copy()
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            m_list[idx] = element
+            return m_list
